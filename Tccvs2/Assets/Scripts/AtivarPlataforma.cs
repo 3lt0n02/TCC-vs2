3d5dc35738a738e8608801f0ddbd61aecf66ef3c
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-07-12 18:46:20 -0300

    06

---
+++ Tccvs2/Assets/Scripts/AtivarPlataforma.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AtivarPlataforma : MonoBehaviour
+{
+    public GameObject plataforma;
+
+    private bool estaPressionado;
+
+    void Start()
+    {
+        plataforma.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (estaPressionado)
+        {
+            plataforma.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            estaPressionado = true;
+        }
+    }
+}
---
+++ Tccvs2/Assets/Scripts/MoverPlataforma.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPlataforma : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public bool plat1, plat2;
+    public bool moverup = true, movedown = true; 
+    void Update()
+    {
+        
+    }
+}
