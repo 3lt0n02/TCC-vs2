b04b0e4704c72014777017904a93168bb24549d3
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-10-19 20:27:11 -0300

    18.2.2

---
+++ Tccvs2/Assets/Codigos/Cenario/Mecanicas_Plataformas/Barreira_De_Morte.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barreira_De_Morte : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
