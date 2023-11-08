41ba8f72e7d17d3ac5a999917c4d875ba58bea3f
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-11-08 11:39:01 -0300

    Correção - pagando scrpites desnecessarios - e conferindo perfomace

--- The Jorney Of Audrey/Assets/Codigos/Inimigos/Armadilhas.cs
+++ The Jorney Of Audrey/Assets/Codigos/Inimigos/Armadilhas.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Armadilhas : MonoBehaviour
 {
     public int dano = 10;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
