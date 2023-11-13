0cfb26bfba8fb51f9cfd8ae5489cfa2213954e80
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-11-13 12:44:52 -0300

    logia de movimento e começa da de ataque do tatu mina

---
+++ The Jorney Of Audrey/Assets/AreaDeAtaque.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class AreaDeAtaque : MonoBehaviour
+{
+    public GameObject tatu; // Referência ao GameObject do Tatu
+    public float aumentoVelocidade = 5.0f; // Aumento de velocidade desejado
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && tatu != null)
+        {
+            Animator tatuAnimator = tatu.GetComponent<Animator>();
+            if (tatuAnimator != null)
+            {
+                // Ativar a animação "Atacando" no componente Animator do objeto Tatu.
+                tatuAnimator.SetBool("Atacando", true);
+
+                // Aumentar a velocidade do Tatu.
+                MovimentoDoTatu movimentoDoTatu = tatu.GetComponent<MovimentoDoTatu>();
+                if (movimentoDoTatu != null)
+                {
+                    movimentoDoTatu.AumentarVelocidade(aumentoVelocidade);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && tatu != null)
+        {
+            Animator tatuAnimator = tatu.GetComponent<Animator>();
+            if (tatuAnimator != null)
+            {
+                // Desativar a animação "Atacando" quando o jogador sai da área de ataque.
+                tatuAnimator.SetBool("Atacando", false);
+
+                // Redefinir a velocidade do Tatu.
+                MovimentoDoTatu movimentoDoTatu = tatu.GetComponent<MovimentoDoTatu>();
+                if (movimentoDoTatu != null)
+                {
+                    movimentoDoTatu.ResetarVelocidade();
+                }
+            }
+        }
+    }
+}
---
+++ The Jorney Of Audrey/Assets/Codigos/Inimigos/Movimento/MovimentoDoTatu.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class MovimentoDoTatu : MonoBehaviour
+{
+    public Transform pontoA;
+    public Transform pontoB;
+    public float velocidade;
+    public float velocidadePadrao = 3.0f;
+    private float velocidadeAtual;
+
+    private bool moverParaEsquerda = true;
+    private Vector3 _olharDireita;
+    private Vector3 _olharEsquerda;
+
+    void Start()
+    {
+        _olharDireita = transform.localScale;
+        _olharEsquerda = transform.localScale;
+        _olharEsquerda.x = _olharEsquerda.x * -1;
+    }
+
+    void Update()
+    {
+        Movimentar();
+    }
+
+    void Movimentar()
+    {
+        if (transform.position.x > pontoA.position.x)
+        {
+            moverParaEsquerda = true;
+        }
+
+        if (transform.position.x < pontoB.position.x)
+        {
+            moverParaEsquerda = false;
+        }
+
+        if (moverParaEsquerda)
+        {
+            transform.Translate(Vector2.left * (velocidade * Time.deltaTime));
+            // Aplicar escala invertida para olhar para a esquerda.
+            transform.localScale = _olharEsquerda;
+        }
+        else
+        {
+            transform.Translate(Vector2.right * (velocidade * Time.deltaTime));
+            // Restaurar a escala original para olhar para a direita.
+            transform.localScale = _olharDireita;
+        }
+       
+    }
+    public void AumentarVelocidade(float aumento)
+    {
+        velocidadeAtual = velocidadePadrao + aumento;
+    }
+
+    public void ResetarVelocidade()
+    {
+        velocidadeAtual = velocidadePadrao;
+    }
+}
