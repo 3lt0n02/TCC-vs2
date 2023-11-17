715b794a406dd908a26105d797b97e939c394b2d
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-11-17 16:56:55 -0300

    Finalização Primiera

--- The Jorney Of Audrey/Assets/Codigos/Inimigos/Vida/MoteDoInimigo.cs
+++ The Jorney Of Audrey/Assets/Codigos/Inimigos/Vida/MoteDoInimigo.cs
@@ -6,28 +6,19 @@ namespace Codigos.Inimigos.Vida
     {
         public float vidaAtual = 100;
         public GameObject barreia;
-        public bool temBarreira = false;
         public void DanoNoImigo(int dano)
         {
             vidaAtual -= dano;
             if (vidaAtual <= 0)
             {
                 Destroy(this.gameObject);
-            }
-        }
-
-        private void Update()
-        {
-            if (temBarreira)
-            {
                 if (barreia != null)
                 {
-                    if (vidaAtual <= 0)
-                    {
-                        barreia.SetActive(false);
-                    } 
+                    barreia.SetActive(false);
                 }
+                
             }
         }
+        
     }
 }
