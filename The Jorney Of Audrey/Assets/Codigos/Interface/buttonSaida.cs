6806ef92735b183f0bea4044f59454c7a7f4e156
Elton da Fonseca Candido <elton.candido@academico.ifrn.edu.br>
2023-11-17 22:30:00 -0300

    Acaba pelo amor de Deus

--- The Jorney Of Audrey/Assets/Codigos/Interface/buttonSaida.cs
+++ The Jorney Of Audrey/Assets/Codigos/Interface/buttonSaida.cs
@@ -1,9 +1,18 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Codigos.Interface
 {
     public class buttonSaida : MonoBehaviour
     {
+        public Button _Button;
+
+        private void Update()
+        {
+            _Button.onClick.AddListener(saida);
+        }
+
         public void saida()
         {
             gamemanager.instance.saida();
